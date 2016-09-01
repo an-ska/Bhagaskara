ce5b80d53ac6aaf0346a022e1776a5c283c4ce52
an-ska <1.anna.piotrowska@gmail.com>
2016-09-01 13:35:56 +0200

    jQuery, class names, colours corrected

--- js/app.js
+++ js/app.js
@@ -1,58 +1,44 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
 
 //progress bars
-    $('.our_skills').on('mouseenter', function() {
-        $('.skillbar').each(function(){
-        	$(this).find('.skillbar_bar').animate({
+    $('.our-skills').on('mouseenter', function() {
+        $('.skillbar').each(function() {
+        	$(this).find('.skillbar-bar').animate({
         		width:$(this).attr('data-percent')
         	}, 2000);
         });
     });
 
 // bottom navigation
-    var navBottomLinks = $('#nav_bottom li').find('a');
-    // console.log(navBottomLinks);
-    navBottomLinks.on('click', function(){
-        navBottomLinks.css('color', 'black');
-        $(this).css('color', 'grey');
+    var navBottomLinks = $('#nav-bottom li').find('a');
+    navBottomLinks.on('click', function() {
+        navBottomLinks.removeClass('navBottomLinksClick');
+        $(this).addClass('navBottomLinksClick')
     });
 
 //filters in gallery
-    var galleryHeadings = $('.portfolio_filter');
-    // console.log(galleryFilters);
-    galleryHeadings.on('click', function(){
-        galleryHeadings.css({
-            'background': 'white',
-            'border': '2px solid black',
-            'color': 'black',
-            'font-weight': 'normal'
-        });
-        $(this).css({
-            'background': 'purple',
-            'border': ' 2px solid purple',
-            'color': 'white',
-            'font-weight': 'bold'
-        });
+    var galleryHeadings = $('.portfolio-filter');
+    galleryHeadings.on('click', function() {
+        galleryHeadings.removeClass('portfolio-filter-click');
+        $(this).addClass('portfolio-filter-click');
     })
 
 // gallery
-    $('.portfolio_filter').on('click', function() {
+    $('.portfolio-filter').on('click', function() {
         var galleryFilters = $(this).attr('data-filter');
-        // console.log(filter);
         if (galleryFilters == 'all') {
-            $('.gallery_pic_selection').removeClass('gallery_pic_hide');
+            $('.gallery-pic-selection').removeClass('gallery-pic-hide');
          } else {
-            $('.gallery_pic_selection').addClass('gallery_pic_hide');
-            $('.gallery_pic_selection[data-filter=' + galleryFilters + ']').removeClass('gallery_pic_hide');
-            $('.gallery_pic_selection[data-filter=' + galleryFilters + ']').next().removeClass('gallery_pic_hide');
-            $('.gallery_pic_selection[data-filter=' + galleryFilters + ']').next().next().removeClass('gallery_pic_hide');
+            $('.gallery-pic-selection').addClass('gallery-pic-hide');
+            $('.gallery-pic-selection[data-filter=' + galleryFilters + ']').removeClass('gallery-pic-hide');
+            $('.gallery-pic-selection[data-filter=' + galleryFilters + ']').next().removeClass('gallery-pic-hide');
+            $('.gallery-pic-selection[data-filter=' + galleryFilters + ']').next().next().removeClass('gallery-pic-hide');
         };
     });
 
 //slow scrolling - main, top navigation
     var navTop = $('#home').find('a');
-    // console.log(navTop);
     navTop.on('click', function(){
         var hrefValue = $(this).attr('href');
         var hrefValueOffset = $(hrefValue).offset();
@@ -61,51 +47,43 @@ $(document).ready(function(){
 
 
 // slow scrolling - bottom navigation
-    var navBottom = $('#nav_bottom').find('a');
-    // console.log(navBottom);
-    navBottom.on('click', function(){
+    var navBottom = $('#nav-bottom').find('a');
+    navBottom.on('click', function() {
         var hrefValue = $(this).attr('href');
         var hrefValueOffset = $(hrefValue).offset();
         $('body, html').animate({scrollTop: hrefValueOffset.top}, 'slow');
     });
 
 //slow scrolling - bottom right arrow
-    $("a[href='#toTop']").on('click', function() {
-    $('body, html').animate({ scrollTop: 0 }, 'slow');
+    $("a[href='#to-top']").on('click', function() {
+    $('body, html').animate({scrollTop: 0}, 'slow');
     });
 
 });
 
 // slider
-document.addEventListener("DOMContentLoaded", function(){
-    var buttonNext = document.querySelector('.team_arrow_right');
-        var buttonPrev = document.querySelector('.team_arrow_left');
-        var list = document.querySelectorAll(".slider li");
+document.addEventListener('DOMContentLoaded', function() {
+    var buttonNext = document.querySelector('.team-arrow-right');
+        var buttonPrev = document.querySelector('.team-arrow-left');
+        var list = document.querySelectorAll('.slider li');
         var picIndex = 0;
+        list[picIndex].className = 'visible';
 
-        console.log(buttonNext);
-        console.log(buttonPrev);
-        console.log(list);
-        console.log(picIndex);
-        list[picIndex].className = "visible";
-
-        buttonNext.addEventListener("click", function(event) {
-            // console.log("I was clicked");
-            list[picIndex].classList.remove("visible");
+        buttonNext.addEventListener('click', function(event) {
+            list[picIndex].classList.remove('visible');
             picIndex++;
             if (picIndex > list.length - 1) {
                 picIndex = list.length - 1;
             }
-            list[picIndex].className = "visible";
+            list[picIndex].className = 'visible';
 
         });
-        buttonPrev.addEventListener("click", function(event) {
-            // console.log("I was clicked as well");
-            list[picIndex].classList.remove("visible");
+        buttonPrev.addEventListener('click', function(event) {
+            list[picIndex].classList.remove('visible');
             picIndex--;
             if (picIndex < 0) {
                 picIndex = 0;
             }
-            list[picIndex].className = "visible";
+            list[picIndex].className = 'visible';
         });
 });
